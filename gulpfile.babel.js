fcbbdaebae4357809ed2f467f56571932ce7103b
Bobby Lee <ldg@dgit.co>
2022-08-01 22:04:55 +0900

    add pug compilation and webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,44 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+import del from "del";
+import connect from "gulp-connect";
+//import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
 };
 
-export const pug = () =>
-  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest)).pipe(connect.reload());
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
 
-export const dev = gulp.series([clean, pug]);
+//const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const webserver = () => {
+  connect.server({
+    root: "build",
+    livereload: true,
+    open: true,
+  });
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
+};
+
+const detectChange = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
+const prepare = gulp.series([clean]);
+
+const asstes = gulp.series([pug]);
+
+const postDev = gulp.series([webserver]);
+
+const watch = gulp.series([detectChange]);
+
+export const dev = gulp.series([prepare, asstes, postDev, watch]);
